c2cbe9d7b6513792b4cbed1e84993f26e2cb12c9
Jeff Simonitto <jsimonitto@gmail.com>
2020-07-26 13:13:48 -0400

    Adding a legend

--- ch2.py
+++ ch2.py
@@ -1,12 +1,4 @@
-from pylab import plot, show
-
-x_numbers = [1, 2, 3]
-y_numbers = [2, 4, 6]
-
-nyc_temp = [53.9, 56.3, 56.4, 53.4, 54.5, 55.8,
-            56.8, 55.0, 55.3, 54.0, 56.7, 56.4, 57.3]
-
-years = range(2000, 2013)
+from pylab import plot, show, legend
 
 nyc_temp_2000 = [31.3, 37.3, 47.2, 51.0, 63.5,
                  71.3, 72.3, 72.7, 66.0, 57.0, 45.3, 31.1]
@@ -19,12 +11,9 @@ nyc_temp_2012 = [37.3, 40.9, 50.9, 54.8, 65.1,
 
 months = range(1, 13)
 
-# plot(x_numbers, y_numbers, marker='o')
-# plot(years, nyc_temp, marker='o')
-# plot(months, nyc_temp_2000, months, nyc_temp_2006, months, nyc_temp_2012)
-# or
 plot(months, nyc_temp_2000)
 plot(months, nyc_temp_2006)
 plot(months, nyc_temp_2012)
+legend([2000, 2006, 2012])
 
 show()
