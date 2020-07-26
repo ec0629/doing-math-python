3b8f800afc795963ea65939eb5c440b8b08eb28f
Jeff Simonitto <jsimonitto@gmail.com>
2020-07-26 11:47:55 -0400

    initial commit

---
+++ ch2.py
@@ -0,0 +1,11 @@
+from pylab import plot, show
+
+x_numbers = [1, 2, 3]
+y_numbers = [2, 4, 6]
+
+nyc_temp = [53.9, 56.3, 56.4, 53.4, 54.5, 55.8,
+            56.8, 55.0, 55.3, 54.0, 56.7, 56.4, 57.3]
+
+#plot(x_numbers, y_numbers, marker='o')
+plot(nyc_temp, marker='o')
+show()
